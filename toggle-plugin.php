b512d1b74601ba0f7670a343bd1303d287e66040
ghilo17 <61m37.17@gmail.com>
2016-06-07 21:22:26 +0800

    Add files via upload

--- toggle-plugin.php
+++ toggle-plugin.php
@@ -1,5 +1,5 @@
 <?php
-/** Plugin Name: iToggle
+/** Plugin Name: gi-Toggle
 * Plugin URI:  https://github.com/ghilo17/toggle-plugin
 * Description: A Slider Toggle used for FAQ's (Wordpress Plugin)
 * Version: 1.5.0
@@ -12,28 +12,19 @@
 
 //Start the function
 
-add_action('wp_enqueue_scripts', 'itoggle');
+add_action('wp_enqueue_scripts', 'gitoggle');
 
-global $post;
+
+
+function gitoggle(){
+    global $post;
     $mouseevent = (get_option('mouse_event') != '') ?  get_option('mouse_event'): 'click';
-    $base_class = (get_option('itoggle_bclass') != '') ? get_option('itoggle_bclass') : 'question';
-    $ansfield_class  = (get_option('itoggle_ansclass') != '') ? get_option('itoggle_ansclass') : 'answer' ;
-    $gen_ansfield  = (get_option('itoggle_ansclass') != '') ? get_option('itoggle_ansclass') : 'answer' ;
-    $ans_count = (get_option('itoggle_anscount') != 0) ? get_option('itoggle_anscount') : 1 ;    
+    $base_class = (get_option('gitoggle_bclass') != '') ? get_option('gitoggle_bclass') : 'question';
+    $ansfield_class  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $gen_ansfield  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $ans_count = (get_option('gitoggle_anscount') != 0) ? get_option('gitoggle_anscount') : 1 ;    
     $page_id  = (get_option('page_id') != '') ? get_option('page_id') : 2 ;
 
- $itoggle_html =  '<div class="itoggle-page'.$page_id.'">' .  . '</div>';
-    // echo $itoggle_html;
-function itoggle_html_code(){
-   $itoggle_html_concat = '';
-    for ($ref=1; $ref <= $ans_count; $ref++) { 
-        $itoggle_html_concat = '<h2 class="'. $base_class . $ref .'">Question</h2>
-            <p class="'. $ansfield_class . $ref .' ' . $gen_ansfield .'">Answer</p>';
-
-    }
-}
-
-function itoggle(){
     if ($post->ID == $page_id) {
         echo '<script src="https://code.jquery.com/jquery-2.2.0.js"></script>';
         toggleAnswer($ans_count, $base_class, $ansfield_class, $ansfield_class, $mouseevent);
@@ -47,6 +38,7 @@ $(document).ready(function(){
 <?php 
 
 	echo '$(".'.$general_class_name_of_answer.'").css("display", "none");';	
+    echo '$(".'.$base_class_name.'").css("cursor", "pointer");';
 for ($i=1; $i <= $num_ans; $i++) {	
 	echo '
 	var pa = ".'.$base_class_name.'";
@@ -63,11 +55,11 @@ for ($i=1; $i <= $num_ans; $i++) {
 <?php
 }
 
-add_action('admin_menu', 'itoggle_plugin_settings');
+add_action('admin_menu', 'gitoggle_plugin_settings');
 
-function itoggle_plugin_settings() {
+function gitoggle_plugin_settings() {
     //creecho ate new top-level menu
-    add_menu_page('iToggle', 'iToggle', 'administrator', 'itoggle_settings', 'itoggle_display_settings', plugin_dir_url( __FILE__ ) . 'itoggle.png', 5);
+    add_menu_page('giToggle', 'giToggle', 'administrator', 'gitoggle_settings', 'gitoggle_display_settings', plugin_dir_url( __FILE__ ) . 'gitoggle.png', 5);
 }
 
 //Admin Style Sheet
@@ -76,27 +68,50 @@ function admin_register_head() {
     $url = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__)) . '/css/style.css';
     echo "<link rel='stylesheet' type='text/css' href='$url' />\n";
 }
+
+//Generate HTML Code
+function gitoggle_html_code(){
+    global $post;
+    $mouseevent = (get_option('mouse_event') != '') ?  get_option('mouse_event'): 'click';
+    $base_class = (get_option('gitoggle_bclass') != '') ? get_option('gitoggle_bclass') : 'question';
+    $ansfield_class  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $gen_ansfield  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $ans_count = (get_option('gitoggle_anscount') != 0) ? get_option('gitoggle_anscount') : 1 ;    
+    $page_id  = (get_option('page_id') != '') ? get_option('page_id') : 2 ;
+
+   $gitoggle_html_concat = '';
+    for ($ref=1; $ref <= $ans_count; $ref++) { 
+        $gitoggle_html_concat =  $gitoggle_html_concat . '<h2 class="'. $base_class . $ref .' ' . $base_class . '">Question</h2>
+            <p class="'. $ansfield_class . $ref .' ' . $gen_ansfield .'">Answer</p>';
+    }
+    return $gitoggle_html =  '<div class="gitoggle-page'.$page_id.'">' . $gitoggle_html_concat . '</div>';
+}
+
 add_action('admin_head', 'admin_register_head');
 
-function itoggle_display_settings() {      
+function gitoggle_display_settings() {      
 
     $mouse_over = (get_option('mouse_event') == 'mouseover') ? 'selected' : '';
     $mouse_click = (get_option('mouse_event') == 'click') ? 'selected' : '';
-    $base_class = (get_option('itoggle_bclass') != '') ? get_option('itoggle_bclass') : 'question';
-    $ansfield_class  = (get_option('itoggle_ansclass') != '') ? get_option('itoggle_ansclass') : 'answer' ;
-    $gen_ansfield  = (get_option('itoggle_ansclass') != '') ? get_option('itoggle_ansclass') : 'answer' ;
-    $ans_count = (get_option('itoggle_anscount') != 0) ? get_option('itoggle_anscount') : 1 ;
-    $page_id  = (get_option('page_id') != '') ? get_option('page_id') : 2 ;
+    $base_class = (get_option('gitoggle_bclass') != '') ? get_option('gitoggle_bclass') : 'question';
+    $ansfield_class  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $gen_ansfield  = (get_option('gitoggle_ansclass') != '') ? get_option('gitoggle_ansclass') : 'answer' ;
+    $ans_count = (get_option('gitoggle_anscount') != 0) ? get_option('gitoggle_anscount') : 1 ;
+    $page_id  = (get_option('page_id') != '') ? get_option('page_id') : 2 ;    
 
-    $html = '<div class="wrap">
 
+    $html = '<div class="wrap" id="gitoggle_admin">
+            <h3>giToggle Wordpress Plugin</h3>
             <form method="post" name="options" action="options.php">
 
             <h2>Select Your Settings</h2>' . wp_nonce_field('update-options') . '
-            <table width="40%" cellpadding="10">
+            <table width="70%" cellpadding="10">
                 <tr>
                     <td align="left" scope="row">
-                    <label>Mouse Event:</label>                             
+                    <div class="help_label">
+                    <label class="label">Mouse Event:</label>
+                    <p class="help">How Answers be toggled by the mouse events..</p>
+                    </div>                             
                     </td> 
                     <td align="left" scope="row">
                     <select name="mouse_event" >
@@ -106,54 +121,70 @@ function itoggle_display_settings() {
                     </td>
                 </tr>
                 <tr>
-                    <td align="left" scope="row">
-                    <label>Base Class Name:</label>
+                    <td align="left" scope="row">                    
+                    <div class="help_label">
+                    <label class="label">Base Class Name:</label>
+                    <p class="help">The Class Name for the Question Field</p>
+                    </div>
                     </td> 
                     <td>
-                    <input type="text" name="itoggle_bclass" 
+                    <input type="text" name="gitoggle_bclass" 
                     value="' . $base_class . '" />
                     </td>                
                 </tr>
                 <tr>
-                    <td align="left" scope="row">
-                    <label>Answer Class Name:</label>
+                    <td align="left" scope="row">                
+                    <div class="help_label">
+                    <label class="label">Answer Class Name:</label>
+                    <p class="help">Class name for the answer field</p>
+                    </div>
                     </td> 
                     <td>
-                    <input type="text" name="itoggle_ansclass" 
+                    <input type="text" name="gitoggle_ansclass" 
                     value="' . $ansfield_class . '" />
                     </td>
                 </tr>
                 <tr>
                     <td align="left" scope="row">
-                    <label>Toggle Count:</label>
+                    <div class="help_label">
+                    <label class="label">Toggle Count:</label>
+                    <p class="help">How Many Answers to be toggled?</p>
+                    </div>
                     </td> 
                     <td>
-                    <input type="number" min="0" name="itoggle_anscount" 
+                    <input type="number" min="0" name="gitoggle_anscount" 
                     value="' . $ans_count . '" />
                     </td>
                 </tr>	
                 <tr>
                     <td align="left" scope="row">
-                    <label>Page Name:</label>
+                    <div class="help_label">
+                    <label class="label">Page Name:</label>
+                    <p class="help">The specific page you will use the toggle</p>
+                    </div>
                     </td> 
                     <td>
                     '. wp_dropdown_pages(). '
                     </td>
-                </tr>
+                </tr>   
                 <tr>
                     <td align="left" scope="row">
-                    <label>HTML Code:</label>
+                    <div class="help_label">
+                    <label class="label">HTML Code:</label>
+                    <p class="help">Copy the code and paste it on the page that you set on the settings, You can also 
+                    Edit the html code according to the question and answer you need.</p>
+                    </div>
                     </td> 
                     <td>
-                    <textarea>
-                    '. itoggle_html_code() .'
+                    <textarea name="gitoggle_html">
+                    '. gitoggle_html_code(). '
                     </textarea>
                     </td>
-                </tr>
+                </tr>                     
             </table>
             <p class="submit">
                 <input type="hidden" name="action" value="update" />  
-                <input type="hidden" name="page_options" value="itoggle_bclass,mouse_event,itoggle_ansclass,itoggle_anscount, page_id" /> 
+                <input type="hidden" name="page_options" value="gitoggle_bclass,mouse_event,gitoggle_ansclass,gitoggle_anscount, gitoggle_html, page_id" /> 
                 <input type="submit" name="Submit" value="Update" style="color: #fff; background: #19A0E2; padding: 7px 20px; border-radius: 7px;
     border: none;"/>
             </p>
